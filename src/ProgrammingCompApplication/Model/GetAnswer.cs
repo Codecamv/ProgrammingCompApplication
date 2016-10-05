3af3391c9647bcc855ae13b303398f1fc3703022
Stig-Johnny <saintjon75@hotmail.com>
2016-10-05 20:54:30 +0200

    Removed dnxcore50. Added ncalc.

--- src/ProgrammingCompApplication/Model/GetAnswer.cs
+++ src/ProgrammingCompApplication/Model/GetAnswer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NCalc;
 
 namespace ProgrammingCompApplication.Model
 {
@@ -13,22 +14,31 @@ namespace ProgrammingCompApplication.Model
             {
                 return "answer";
             }
+            
+            //else if (question == "Calculate this formula: 3 + 2")
+            //{
+            //    return "5";
+            //}
+            
+            //else if (question == "Calculate this formula: 3 + 7")
+            //{
+            //    return "12";
+            //}
             // Q2FsY3VsYXRlIHRoaXMgZm9ybXVsYTogMyArIDI=
-            else if (question == "Calculate this formula: 3 + 2")
-            {
-                return "5";
-            }
             // Q2FsY3VsYXRlIHRoaXMgZm9ybXVsYTogMyArIDc=
-            else if (question == "Calculate this formula: 3 + 7")
-            {
-                return "12";
-            }
             // Q2FsY3VsYXRlIHRoaXMgZm9ybXVsYTogU3FydCg0KQ==
             else if (question.Contains("Calculate this formula:"))
             {
                 // https://ncalc.codeplex.com/
                 question = question.Replace("Calculate this formula:", "").Replace(" ", "");
-                // can't get ncalc to work on asp.net core
+                try
+                {
+                    return new Expression(question).Evaluate().ToString();
+                }
+                catch (Exception)
+                {
+                    return "";
+                }
             }
 
             return "";
